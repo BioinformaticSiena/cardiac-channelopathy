666994eca1ea521969279f48706c89e8eb959911
aldone1 <64198779+aldone1@users.noreply.github.com>
2025-05-06 02:38:13 +0200

    Delete src/AldoSCN5A_analisi.py

--- src/AldoSCN5A_analisi.py
+++
@@ -1,8 +0,0 @@
-
-from Bio import SeqIO
-from Bio.Align import fasta
-from Bio.SeqUtils import gc_fraction
-# Caricamento della sequenza dal file FASTA
-record = SeqIO.read('/data/AldoSCN5A.fasta', "fasta")
-seq = record.seq
-print("Lunghezza della sequenza:", len(seq))
